fad619269bca107f27a9359d1cee4984d5d81e92
ViktarTilichkin <tilichkin@mail.ru>
2023-02-28 22:51:08 +0300

    update

--- Games.Core/GamesCore.cs
+++ Games.Core/GamesCore.cs
@@ -7,27 +7,7 @@ namespace Games.Core
 {
     public class GamesCore
     {
-        // отсюда будем вызывать
-
-        // функцианал коре 
-
-        // принимать поля id для ведения статистики игры 
-        // подключаться к репозиторию статистики и писать туда статистику по текущей игре 
-        // отдельный фаил для породолжения игры !
-        // - просмотр статистики игрока
-        // - статистика по всем играм ??
-
-        // интрфейс игры
-        // - старт +
-        // - старт из сохранения если такое есть +
-        // - количество игроков +
-        //public string user { get; private set; } 
-        //public string  Stats = new UserStatsRepository();
-
-        //поулчить имя пользователя id
-        //    поулчить игру 
-
-        //    и вызываем игру
+        
         private XO games = new XO();
         public int IdUser { get; set; }
 
@@ -36,8 +16,9 @@ namespace Games.Core
             IdUser = idUser;
         }
 
-        public void MenuGames(string userName, out string gameResult)
+        public void MenuGames(int id, string userName, out string gameResult)
         {
+            IdUser = id;
             gameResult = null;
             Console.WriteLine($"Hi! {userName}");
             Console.WriteLine($"What game do you want to play?");
---
+++ Games.Shared/SaveGameRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Games.Shared
+{
+    public class SaveGameRepository<T>
+    {
+        protected string path { get; }
+        protected IEnumerable<T> GetAll()
+        {
+            var serializeoptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            };
+            using StreamReader sr1 = new StreamReader(path);
+            for (string line = sr1.ReadLine(); line != null; line = sr1.ReadLine())
+            {
+                yield return JsonSerializer.Deserialize<T>(line, serializeoptions);
+
+            }
+            sr1.Close();
+
+
+        }
+        protected void UpdateFile(List<T> dataList)
+        {
+            try
+            {
+                var serializeoptions = new JsonSerializerOptions
+                {
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                };
+                StreamWriter sw1 = new StreamWriter(path);
+                for (int i = 0; i < dataList.Count; i++)
+                {
+                    if (dataList[i] != null)
+                    {
+                        string json = JsonSerializer.Serialize(dataList[i], serializeoptions);
+                        sw1.WriteLine(json);
+                    }
+                }
+                sw1.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
